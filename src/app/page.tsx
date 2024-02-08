7961e13708390c2c2a82ab4f7d39aaf8d4d41af6
Kevin Shirley <kevinxshirley@gmail.com>
2024-02-08 18:45:45 -0500

    Slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Slider from '@/components/slider'
+
 export default function Home() {
   return (
-    <main className='relative bg-slate-900 min-h-screen'>
-      <div className='text-white'>
-        test
+    <main className='relative bg-gray-200 min-h-screen py-6'>
+      <div className='py-2 px-6 mb-12'>
+        <div className='mb-12'>
+          <h2>1. How many pages your project will have?</h2>
+        </div>
+        <Slider min={0} max={20} />
+      </div>
+      <div className='py-2 px-6 mb-12'>
+        <div className='mb-12'>
+          <h2>2. Which design service your company needs?</h2>
+        </div>
+        <Slider min={0} max={20} />
       </div>
     </main>
   );
---
+++ src/components/slider/index.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React, { useState } from 'react';
+import { Slider } from 'antd';
+
+interface CommonSliderProps {
+  max: number;
+  min: number;
+}
+
+const CommonSlider: React.FC<CommonSliderProps> = (props) => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Slider
+      defaultValue={1}
+      onChange={setValue}
+      value={value}
+      {...props}
+    />
+  );
+}
+
+export default CommonSlider;
