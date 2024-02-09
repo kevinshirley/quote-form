0f70100f72e0d4976e8a81e14978fc00183b64e6
Kevin Shirley <kevinxshirley@gmail.com>
2024-02-08 20:38:25 -0500

    Card component + design service section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
 import Slider from '@/components/slider'
+import Card from '@/components/card'
 
 export default function Home() {
   return (
     <main className='relative bg-gray-200 min-h-screen py-6'>
-      <div className='py-2 px-6 mb-12'>
+      <div className='py-2 px-6 mb-20'>
         <div className='mb-12'>
-          <h2>1. How many pages your project will have?</h2>
+          <h2 className='font-semibold text-xl'>1. How many pages your project will have?</h2>
         </div>
-        <Slider min={0} max={20} />
+        <Slider min={1} max={20} />
       </div>
-      <div className='py-2 px-6 mb-12'>
+      <div className='py-2 px-6 mb-20'>
         <div className='mb-12'>
-          <h2>2. Which design service your company needs?</h2>
+          <h2 className='font-semibold text-xl'>2. Which design service your company needs?</h2>
         </div>
-        <Slider min={0} max={20} />
+        <div className='flex flex-col items-center'>
+          <Card
+            title='None'
+            text='I already have a design'
+          />
+          <Card
+            title='New Design'
+            text='Custom design with style guide'
+          />
+          <Card
+            title='Template'
+            text='I want to use an existing template'
+          />
+        </div>
       </div>
     </main>
   );
