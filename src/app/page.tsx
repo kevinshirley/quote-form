6ac10c5d86c775d1c7dd95868107cf6f0ea5e848
Kevin Shirley <kevinxshirley@gmail.com>
2024-02-09 18:04:13 -0500

    QuotePrice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,11 @@
-import { Button } from 'antd'
-import Slider from '@/components/slider'
-import Card from '@/components/card'
-import FormItem from '@/components/form-item'
-import AdditionalServices from '@/components/form-flow/additional-services'
-import ContactInformation from '@/components/form-flow/contact-information'
+import FormFlow from '@/components/form-flow'
+import QuotePrice from '@/components/quote-price'
 
 export default function Home() {
   return (
     <main className='relative bg-gray-200 min-h-screen py-6'>
-      <FormItem title='1. How many pages your project will have?'>
-        <Slider min={1} max={20} />
-      </FormItem>
-      <FormItem title='2. Which design service your company needs?'>
-        <div className='flex flex-col items-center'>
-          <Card
-            title='None'
-            text='I already have a design'
-          />
-          <Card
-            title='New Design'
-            text='Custom design with style guide'
-          />
-          <Card
-            title='Template'
-            text='I want to use an existing template'
-          />
-        </div>
-      </FormItem>
-      <FormItem title='3. Which development service your company needs?'>
-        <div className='flex flex-col items-center'>
-          <Card
-            title='New Website Development'
-            text=''
-          />
-          <Card
-            title='Existing Website Migration'
-            text=''
-          />
-          <Card
-            title='Other'
-            text=''
-          />
-        </div>
-      </FormItem>
-      <FormItem title='4. Would you like to use animations on your website?'>
-        <div className='flex flex-col items-center'>
-          <Card
-            title='None'
-            text='Static page with no animations'
-          />
-          <Card
-            title='Basic'
-            text='Basic fade in / fade out animations'
-          />
-          <Card
-            title='Advanced'
-            text='Very unique and customised animations'
-          />
-        </div>
-      </FormItem>
-      <AdditionalServices />
-      <ContactInformation />
-      <FormItem>
-        <Button
-          className='quote-form-submut-btn w-full border-slate-900 border-2 text-slate-900 bg-slate-900 text-white h-14'
-          size='large'
-        >
-          Primary
-        </Button>
-      </FormItem>
+      <QuotePrice />
+      <FormFlow />
     </main>
   );
 }
---
+++ src/components/quote-price/index.tsx
@@ -0,0 +1,10 @@
+export default function QuotePrice() {
+  return (
+    <div className='fixed top-auto bottom-[1.5rem] left-auto right-[1.5rem] z-50 bg-neutral-950/[.01] backdrop-blur-xl py-6 px-10 rounded-md border-gray-300 border-2 h-36 flex justify-center items-center'>
+      <div className='flex flex-col items-center'>
+        <span className='text-xs mb-2'>Approximate price is</span>
+        <span className='text-4xl font-bold'>$0</span>
+      </div>
+    </div>
+  );
+}
