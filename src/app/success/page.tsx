5f54db523a63a658d2fb160965a1f6ee66f5e367
Kevin Shirley <kevinxshirley@gmail.com>
2024-02-13 10:28:19 -0500

    Success White background

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,6 @@
 export default function Success() {
   return (
-    <main className='relative bg-gray-200 min-h-screen py-6'>
+    <main className='relative bg-white min-h-screen py-6'>
       <div className='px-4 py-8'>
         <h1 className='font-bold'>Success! You will receive a confirmation email shortly. A representative will contact you shortly as well.</h1>
       </div>
